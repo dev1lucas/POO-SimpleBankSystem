6e277e790c8bdec81796475bd8aed7aba034d068
Lucas Santos <dev.lucas06@gmail.com>
2025-04-24 23:45:44 -0300

    Primeiro commit

---
+++ SimpleBankSystem/Program.cs
@@ -0,0 +1,4 @@
+﻿var titular = new Titular("Lucas Araújo", "123.456.789-00", "Rua das Flores, 123");
+var conta = new Conta(titular, 1234, 567890, 1500.00, 500.00);
+
+Console.WriteLine(conta.Informacoes);
